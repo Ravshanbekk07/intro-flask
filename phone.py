115d8fd0f43473fd4b62b67ea470c0b4799c0b38
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-14 16:49:06 +0500

    smartphone

---
+++ phone.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from tinydb import TinyDB, Query
+
+app = Flask(__name__)
+
+db = TinyDB('db.json')
+phones = db.table('phones')
+
+@app.route('/brends')
+def brend():
+    if request.method == 'GET':
+        brends = []
+        for phone in phones.all():
+            if phone['brend'] not in brends:
+                brends.append(phone['brend'])
+        return {
+            'brends': brends
+        }
+
+
+@app.route('/phones')
+def get_phones():
+    params = request.args
+    brend = params.get('brend', None)
+    if brend != None:
+        q = Query()
+        return {
+            'phones': phones.search(q.brend == brend)
+        }
+    return {
+        'phones': phones.all()
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
