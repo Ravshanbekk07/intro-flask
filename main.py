37767a309239838cbff622e302708aafac22fdfa
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-14 16:07:12 +0500

    get query parasm value

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/say-hello')
+def say_hello():
+    params = request.args
+    name = params.get('name')
+    return f'Hello <b>{name}</b>!'
 
 
 if __name__ == '__main__':
