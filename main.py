3ad672106a98ccfd414d3b2700f39957869be109
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-14 16:11:01 +0500

    fdfs

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print(request.path)
+    print(request.method)
+    print(request.headers)
+
     return 'Hello World!'
 
 @app.route('/say-hello')
